64865a2e66766dad083f479c9516a2488db13d2d
LBugnion <lbugnion@outlook.com>
2017-09-15 18:01:23 +0200

    Adding sample for the demo

---
+++ Mvvm/HelloXamarin MVVM/UnitTestApp1/TestSample.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using UnitTestApp1.Model;
+using HelloAndroid.ViewModel;
+
+namespace UnitTestApp1
+{
+    [TestFixture]
+    public class TestsSample
+    {
+        [Test]
+        public void MainViewModel_TestRefresh_ShouldSucceed()
+        {
+            var service = new TestYoutubeService();
+            var vm = new MainViewModel(service);
+
+            vm.RefreshCommand.Execute(null);
+
+            Assert.AreEqual(TestYoutubeService.ResultPassed, vm.Result);
+        }
+
+        [Test]
+        public void MainViewModel_TestRefreshFail_ShouldCopyExceptionMessageInResult()
+        {
+            var service = new TestYoutubeService(true);
+            var vm = new MainViewModel(service);
+
+            vm.RefreshCommand.Execute(null);
+
+            Assert.AreEqual(TestYoutubeService.ExceptionMessage, vm.Result);
+        }
+    }
+}
